645c7c1de9b6fdf0e6a633d2a7364685d451ba5e
Fernando Hoyos <fernandolhoyosh@gmail.com>
2025-01-14 23:15:55 -0500

    fix-not found 3

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.put('/videos/:id', (req, res) => {
 
 // Middleware para manejar rutas no encontradas
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'path-to-frontend-build','index.html'));
+  res.status(404).sendFile(path.join(__dirname, 'build','index.html'));
 });
 
 app.listen(PORT, () => {
